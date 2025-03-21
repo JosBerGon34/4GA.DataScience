fbf1da49fe371975a70f849bd0815183968cf478
JosueBertomeuGonzalez <infoworkjbg@gmail.com>
2025-03-21 11:58:28 +0100

    s113

--- src/Diabetes_app.py
+++ src/Diabetes_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from pickle import load
-
-# Cargar el modelo
+#Visitad url de streamlit: mod27jbg.streamlit.app
+#Cargar el modelo
 model = load(open("C:/Users/Josue/4GA.DataScience/models/3c_G_MD4_MF8_MSL4_MSS7.sav", "rb"))
 
-# Variables objetivo y predictoras
+#Variables objetivo y predictoras
 target_var = 'Outcome'
 predict_var1 = 'Pregnancies'
 predict_var2 = 'Glucose'
@@ -13,16 +13,16 @@ predict_var4 = 'BMI'
 predict_var5 = 'DiabetesPedigreeFunction'
 predict_var6 = 'Age'
 
-# Diccionario de clases
+#Diccionario de clases
 class_dict = {
     "0": "No Diabetes",
     "1": "Diabetes"
 }
 
-# Título de la aplicación
+#Título de la aplicación
 st.title("Diabetes Prediction")
 
-# Valores iniciales de las barras deslizantes
+#Valores iniciales de las barras deslizantes
 initial_values = {
     predict_var1: 0,
     predict_var2: 100,
@@ -32,18 +32,18 @@ initial_values = {
     predict_var6: 40
 }
 
-# Función para restablecer los valores
+#Función para restablecer los valores
 def reset_values():
     for var, value in initial_values.items():
         st.session_state[var] = value
 
-# Inicializar st.session_state si no existe
+#Inicializar st.session_state si no existe
 if 'initialized' not in st.session_state:
     for var, value in initial_values.items():
         st.session_state[var] = value
     st.session_state['initialized'] = True
 
-# Barras deslizantes para las variables predictoras
+#Barras deslizantes para las variables predictoras
 val1 = st.slider(predict_var1, min_value=0, max_value=16, step=1, key=predict_var1)
 val2 = st.slider(predict_var2, min_value=25, max_value=200, step=1, key=predict_var2)
 val3 = st.slider(predict_var3, min_value=0, max_value=350, step=1, key=predict_var3)
@@ -51,18 +51,18 @@ val4 = st.slider(predict_var4, min_value=10.0, max_value=60.0, step=0.1, key=pre
 val5 = st.slider(predict_var5, min_value=0.10, max_value=1.4, step=0.01, key=predict_var5)
 val6 = st.slider(predict_var6, min_value=20, max_value=75, step=1, key=predict_var6)
 
-# Botón de predicción
+#Botón de predicción
 if st.button("Predict"):
-    # Realizar la predicción
+    #Realizar la predicción
     prediction = str(model.predict([[val1, val2, val3, val4, val5, val6]])[0])
 
-    # Obtener el nombre de la clase predicha
+    #Obtener el nombre de la clase predicha
     pred_class = class_dict[prediction]
 
-    # Mostrar la predicción
+    #Mostrar la predicción
     st.write("Prediction:", pred_class)
 
-# Botón de reset
+#Botón de reset
 if st.button("Reset"):
     reset_values()
     st.experimental_rerun()
